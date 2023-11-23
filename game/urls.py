4bb149f00907d154409646958bef602ea39ebe69
ApostolApostolov <73019184+ApostolApostolov@users.noreply.github.com>
2023-11-23 11:34:01 +0000

    Update urls.py
    url to stat

--- game/urls.py
+++ game/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
 
-from . import views
+from . import views 
+from .views import(wordPerMinute)
 
 urlpatterns = [
     path('', views.home, name='typing-game-main'),
     path('typing-tutor', views.typing_tutor, name='typing-game-actual'),
+    path('typing-tutor-stat', views.statistic, name='typing-game-stat'),
+    path('typing-tutor-stat-test', wordPerMinute.as_view(), name='typing-game-stat'),
+    
 ]
