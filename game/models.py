0b4c5336f4fa948903b33efdab7f89f3fadeda2b
ApostolApostolov <73019184+ApostolApostolov@users.noreply.github.com>
2023-11-23 11:33:18 +0000

    Added database structure & way to view it in stats

--- game/models.py
+++ game/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+#super User
+# Kiro
+# testing321
+
+
+class wordPerMinute(models.Model):
+    wpm = models.PositiveSmallIntegerField()
+    date_posted = models.DateTimeField(default=timezone.now)
+    player = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    
--- game/views.py
+++ game/views.py
@@ -1,8 +1,25 @@
 from django.shortcuts import render
-
+from .models import wordPerMinute
+from django.views.generic import (ListView)
 # Create your views here.
+
+
+
 def home(request):
     return render(request, 'game/home.html') 
 
 def typing_tutor(request):
     return render(request, 'game/typing_tutor.html') 
+
+def statistic(request):
+    context = {
+        'wordPerMinute': wordPerMinute.objects.all()
+    }
+    return render(request, "game/statistics.html", context)
+
+class wordPerMinute(ListView):
+    model = wordPerMinute
+    template_name = "game/statistics.html"
+    context_object_name = 'wordPerMinute'
+    ordering = ['-wpm']
+    # paginate_by = 50
